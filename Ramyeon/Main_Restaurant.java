d29a6dfeb7f6f05f659cca0b2bee2f4cd3f9cc1c
Kimminsu <kmsa0202@gmail.com>
2024-04-30 17:06:11 +0900

    랜덤추가

--- Ramyeon/Main_Restaurant.java
+++ Ramyeon/Main_Restaurant.java
@@ -1,5 +1,7 @@
 package Ramyeon;
 
+import util.RandomSelectNumber;
+
 public class Main_Restaurant {
 
 	public static void main(String[] args) {
@@ -28,6 +30,13 @@ public class Main_Restaurant {
 		
 		System.out.println("라라라면 완성");
 		
+		String[] eatMembers = {"김민수",
+				"이현종","이수진",
+				"남귀연",	"이연수"};
+		
+		int selectedNumberofMember = RandomSelectNumber.selectInt(7);
+		// 시식자 선정 완료. 
+		System.out.println("시식하는 사람은 : " + eatMembers[selectedNumberofMember] + " 당첨.ㅋㅋㅋ");
 	}
 
 }
