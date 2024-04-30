fc4cc5955b6555c367edd11961a9f9ecb9a33043
ye0nsuuuuu <1eey3onsu@gmail.com>
2024-04-30 15:45:21 +0900

    라면만들기

---
+++ Ramyeon/Main_Restaurant.java
@@ -0,0 +1,10 @@
+package Ramyeon;
+
+public class Main_Restaurant {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Ramyeon/ramyeon_01.java
@@ -0,0 +1,16 @@
+package Ramyeon;
+
+public interface ramyeon_01 {
+	
+	
+	//국물 여부
+	public abstract void soupOrNot(String soup); 
+	//면굵기
+	public abstract void NoodleThickness(int size); 
+	//토핑선택
+	public abstract void Topping(String[] ToppingSources); 
+	//사이드메뉴
+	public abstract void SideMenu(String[] ChooseSideMenu); 
+	
+	
+}
